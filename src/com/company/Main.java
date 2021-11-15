99de82ce51832931713e6273186ed800d80a6168
gareev_mr <maratgareev97@gmail.com>
2021-11-15 15:58:25 +0300

    Изменения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,175 +1,44 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public String mat_v;
+
     public static void main(String[] args) throws IOException {
         Number rim = new Number();
         Scanner in = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String mat_v = in.nextLine();
 
-        mat_v=rim.del_space(mat_v);
+        mat_v = rim.del_space(mat_v); // удаление пробелов из строки
 
-        int i = 0;
-
-        int plus1 = mat_v.indexOf("+");
-        int plus2 = mat_v.lastIndexOf("+");
-        int minus1 = mat_v.indexOf("-");
-        int minus2 = mat_v.lastIndexOf("-");
-        int umn1 = mat_v.indexOf("*");
-        int umn2 = mat_v.lastIndexOf("*");
-        int del1 = mat_v.indexOf("/");
-        int del2 = mat_v.lastIndexOf("/");
-        String[] mas = new String[3];
-        if ((plus1 != -1) && (plus2 == plus1)) {
-            //String[]
-            mas[0] = mat_v.substring(0, plus1);
-            mas[1] = mat_v.substring(plus1, plus1 + 1);
-            mas[2] = mat_v.substring(plus1 + 1);
-            i++;
-        }
-        ;
-        if ((minus1 != -1) && (minus1 == minus2)) {
-            mas[0] = mat_v.substring(0, minus1);
-            mas[1] = mat_v.substring(minus1, minus1 + 1);
-            mas[2] = mat_v.substring(minus1 + 1);
-            i++;
-        }
-        ;
-        if ((umn1 != -1) && (umn1 == umn2)) {
-            mas[0] = mat_v.substring(0, umn1);
-            mas[1] = mat_v.substring(umn1, umn1 + 1);
-            mas[2] = mat_v.substring(umn1 + 1);
-            i++;
-        }
-        if ((del1 != -1) && (del1 == del2)) {
-            mas[0] = mat_v.substring(0, del1);
-            mas[1] = mat_v.substring(del1, del1 + 1);
-            mas[2] = mat_v.substring(del1 + 1);
-            i++;
-        }
-
-        //String mas1 = rim.count_mat(mat_v);
-
-        //System.out.println(mas1+"count"+mas1);
-
+        String mas3[] = rim.del_mat_zn(mat_v); // разбить строку на числа и знак дейсвия, держим в массиве.
+                                                // Первый элемент это проверка: один знак или нет
         int a = 0;
         int b = 0;
-        if (i == 1) {
-            //System.out.println(Arrays.toString(mas));
-            //System.out.println(i + " i");
-            //-----------проверка на арабское число-----------------------
-            int k = 0;
-            String[] nabob1 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-            for (i = 0; i < 10; i++) {
-                //System.out.println(nabob1[i] + " f " + mas[0]);
-                if (mas[0].equals(nabob1[i])) {
-                    k++;
-                }
-                if (mas[2].equals(nabob1[i])) {
-                    k++;
-                }
-                if (k == 2) {
-                    a = Integer.parseInt(mas[0]);
-                    b = Integer.parseInt(mas[2]);
-                }
+        if (Integer.parseInt(mas3[0]) == 1) {
+            int mas1[] = rim.arab(mas3[1], mas3[3]); // проверяем арабские числа пришили или нет.
+            a = mas1[0]; // записываем в переменную первое число
+            b = mas1[1]; // записываем в переменную первое число
+
+            if (rim.in_rimskoe(mas3[1], mas3[3])) { // проверяем римские чила пришили или нет. k1 должно равно быть 2
+                int mas2[] = rim.rimskoe_to_arab(mas3[1], mas3[3]);
+                a = mas2[0];
+                b = mas2[1];
             }
-            //----------------------------------------------------------------------
-            int k1 = 0;
-            //-----------------проверка на рисмкое---------------------------
-            String[] nabob2 = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-            for (i = 0; i < 10; i++) {
-                //System.out.println(nabob1[i] + " f " + mas[0]);
-                if (mas[0].equals(nabob2[i])) {
-                    k1++;
-                }
-                if (mas[2].equals(nabob2[i])) {
-                    k1++;
-                }
-            }
-            if (k1 == 2) {
-                switch (mas[0]) {
-                    case "I":
-                        mas[0] = "1";
-                        break;
-                    case "II":
-                        mas[0] = "2";
-                        break;
-                    case "III":
-                        mas[0] = "3";
-                        break;
-                    case "IV":
-                        mas[0] = "4";
-                        break;
-                    case "V":
-                        mas[0] = "5";
-                        break;
-                    case "VI":
-                        mas[0] = "6";
-                        break;
-                    case "VII":
-                        mas[0] = "7";
-                        break;
-                    case "VIII":
-                        mas[0] = "8";
-                        break;
-                    case "IX":
-                        mas[0] = "9";
-                        break;
-                    case "X":
-                        mas[0] = "10";
-                        break;
-                }
-                switch (mas[2]) {
-                    case "I":
-                        mas[2] = "1";
-                        break;
-                    case "II":
-                        mas[2] = "2";
-                        break;
-                    case "III":
-                        mas[2] = "3";
-                        break;
-                    case "IV":
-                        mas[2] = "4";
-                        break;
-                    case "V":
-                        mas[2] = "5";
-                        break;
-                    case "VI":
-                        mas[2] = "6";
-                        break;
-                    case "VII":
-                        mas[2] = "7";
-                        break;
-                    case "VIII":
-                        mas[2] = "8";
-                        break;
-                    case "IX":
-                        mas[2] = "9";
-                        break;
-                    case "X":
-                        mas[2] = "10";
-                        break;
-                }
-                a = Integer.parseInt(mas[0]);
-                b = Integer.parseInt(mas[2]);
-                //System.out.println(mas[0] + "," + mas[2]);
-            }
-            //-----------------------------------------------------------------------
 
             int s = -1;
             if ((a == 0) || (b == 0)) {
                 throw new IOException();
                 //System.out.println("Нет цифр");
-            } else {
-                switch (mas[1]) {
+            }
+            else {
+                switch (mas3[2]) {
                     case "+":
                         s = a + b;
-                        //System.out.println(a + "," + b + "," + s + ",");
                         break;
                     case "-":
                         s = a - b;
@@ -181,20 +50,18 @@ public class Main {
                         s = a / b;
                         break;
                 }
-                if (k1 == 2) {
-                    if (s > 0) {
-                        System.out.println(rim.rimskoe(s));
-                    }
-                    else {
+                if (rim.in_rimskoe(mas3[1], mas3[3])) { //Если римское вошло
+                    if (s > 0) {                        // и сумма образовалась
+                        System.out.println(rim.rimskoe(s)); // переводим в римские цифры
+                    } else {
                         System.out.println("У римлян нет отрицательных числел");
                         throw new IOException();
                     }
                 } else {
-                    System.out.println(s);
+                    System.out.println(s); //если арабские пришли, то выводим арабскими
                 }
             }
-        }
-        else {
+        } else {
             try {
                 throw new Exception("Если нет знаков математических действий");
             } catch (Exception e) {
--- src/com/company/Number.java
+++ src/com/company/Number.java
@@ -9,8 +9,8 @@ public class Number {
         String s = "";
         int ed;
         int des;
-        if (x==100){
-            return s="C";
+        if (x == 100) {
+            return s = "C";
         }
         ed = x % 10;
         des = x / 10;
@@ -85,7 +85,8 @@ public class Number {
         s = s2 + s1;
         return s;  //возвращаем посчитанное значение
     }
-    public static String del_space(String s){
+
+    public static String del_space(String s) {
         // -----------------------------------Удаление пробелов---------------------------------------------
         StringBuilder mat_v1 = new StringBuilder(s);
         int i = 0;
@@ -100,7 +101,165 @@ public class Number {
         //------------------------------------------------------------------------------------------
     }
 
-    public static String count_mat(String mat_v){
-        return "0";
+    public static int[] arab(String first_num, String second_mun) {
+        int a = 0;
+        int b = 0;
+        //System.out.println(Arrays.toString(mas));
+        //System.out.println(i + " i");
+        //-----------проверка на арабское число-----------------------
+        int k = 0;
+        String[] nabob1 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(nabob1[i] + " f " + mas[0]);
+            if (first_num.equals(nabob1[i])) {
+                k++;
+            }
+            if (second_mun.equals(nabob1[i])) {
+                k++;
+            }
+            if (k == 2) {
+                a = Integer.parseInt(first_num);
+                b = Integer.parseInt(second_mun);
+                break;
+            }
+        }
+        return new int[]{a, b};
+    }
+
+    public static boolean in_rimskoe(String first_num, String second_mun) {
+        int k1 = 0;
+        int a = 0;
+        int b = 0;
+        //-----------------проверка на рисмкое---------------------------
+        String[] nabob2 = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(nabob1[i] + " f " + mas[0]);
+            if (first_num.equals(nabob2[i])) {
+                k1++;
+            }
+            if (second_mun.equals(nabob2[i])) {
+                k1++;
+            }
+        }
+        if (k1==2){
+            return true;
+        }
+    return false;
+    }
+
+    public static int[] rimskoe_to_arab(String first_num, String second_mun) {
+        int a=0;
+        int b=0;
+        switch (first_num) {
+            case "I":
+                first_num = "1";
+                break;
+            case "II":
+                first_num = "2";
+                break;
+            case "III":
+                first_num = "3";
+                break;
+            case "IV":
+                first_num = "4";
+                break;
+            case "V":
+                first_num = "5";
+                break;
+            case "VI":
+                first_num = "6";
+                break;
+            case "VII":
+                first_num = "7";
+                break;
+            case "VIII":
+                first_num = "8";
+                break;
+            case "IX":
+                first_num = "9";
+                break;
+            case "X":
+                first_num = "10";
+                break;
+        }
+        switch (second_mun) {
+            case "I":
+                second_mun = "1";
+                break;
+            case "II":
+                second_mun = "2";
+                break;
+            case "III":
+                second_mun = "3";
+                break;
+            case "IV":
+                second_mun = "4";
+                break;
+            case "V":
+                second_mun = "5";
+                break;
+            case "VI":
+                second_mun = "6";
+                break;
+            case "VII":
+                second_mun = "7";
+                break;
+            case "VIII":
+                second_mun = "8";
+                break;
+            case "IX":
+                second_mun = "9";
+                break;
+            case "X":
+                second_mun = "10";
+                break;
+        }
+        a=Integer.parseInt(first_num);
+        b=Integer.parseInt(second_mun);
+    return new int[]{a, b};
+    }
+
+    public static String[] del_mat_zn(String mat_v) {
+        int i = 0;
+
+        int plus1 = mat_v.indexOf("+");
+        int plus2 = mat_v.lastIndexOf("+");
+        int minus1 = mat_v.indexOf("-");
+        int minus2 = mat_v.lastIndexOf("-");
+        int umn1 = mat_v.indexOf("*");
+        int umn2 = mat_v.lastIndexOf("*");
+        int del1 = mat_v.indexOf("/");
+        int del2 = mat_v.lastIndexOf("/");
+
+        String[] mas = new String[3];
+        if ((plus1 != -1) && (plus2 == plus1)) {
+            //String[]
+            mas[0] = mat_v.substring(0, plus1);
+            mas[1] = mat_v.substring(plus1, plus1 + 1);
+            mas[2] = mat_v.substring(plus1 + 1);
+            i++;
+        }
+        ;
+        if ((minus1 != -1) && (minus1 == minus2)) {
+            mas[0] = mat_v.substring(0, minus1);
+            mas[1] = mat_v.substring(minus1, minus1 + 1);
+            mas[2] = mat_v.substring(minus1 + 1);
+            i++;
+        }
+        ;
+        if ((umn1 != -1) && (umn1 == umn2)) {
+            mas[0] = mat_v.substring(0, umn1);
+            mas[1] = mat_v.substring(umn1, umn1 + 1);
+            mas[2] = mat_v.substring(umn1 + 1);
+            i++;
+        }
+        if ((del1 != -1) && (del1 == del2)) {
+            mas[0] = mat_v.substring(0, del1);
+            mas[1] = mat_v.substring(del1, del1 + 1);
+            mas[2] = mat_v.substring(del1 + 1);
+            i++;
+        }
+
+    return new String[]{String.valueOf(i),mas[0], mas[1],mas[2]};
     }
 }
